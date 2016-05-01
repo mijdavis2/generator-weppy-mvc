0e895863d1ca0d94f1be243daf0b26d049c6d066
mijdavis2 <mdavisinsc@gmail.com>
2016-05-01 14:06:44 -0700

    :new: Add test for application files

--- test/app.js
+++ test/app.js
@@ -2,17 +2,27 @@
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
+var test_app_name = 'name_x';
 
 describe('generator-weppy-mvc:app', function () {
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
-      .withArguments(['name-x'])
+      .withArguments([test_app_name])
       .on('end', done);
   });
 
-  it('creates files', function () {
+  it('creates root files', function () {
     assert.file([
       'run.py'
     ]);
+    assert.fileContent('run.py', test_app_name);
+    assert.noFileContent('run.py', 'app_name');
+  });
+  it('creates application files', function () {
+    assert.file([
+      test_app_name + '/__init__.py'
+    ]);
+    assert.fileContent(test_app_name + '/__init__.py', test_app_name);
+    assert.noFileContent(test_app_name + '/__init__.py', 'app_name');
   });
 });
