62a2534ea47b768cf80c24e4c6cac86ec7bf773f
mjdavis <mjdavis@uber.com>
2016-10-19 12:24:36 -0700

    :up: Update weppy version

--- app/index.js
+++ app/index.js
@@ -111,7 +111,7 @@ module.exports = yeoman.Base.extend({
       license: this.answers.license,
       year: this._defaultYear(),
       includeLicense: this.answers.includeLicense,
-      weppyVersion: '0.7.9'
+      weppyVersion: '0.7.10'
     };
     if (this.answers.useDirectory != this.appname) {
       this.destinationRoot(this.answers.useDirectory);
